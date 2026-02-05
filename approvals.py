6930f1ca488348cd1b3892a68a723cd8fdd1303d
Renn Valo <133272211+RVNOAA@users.noreply.github.com>
2026-02-05 10:22:03 -0700

    Add comment for automated PR creation test
    Add comment for automated PR creation test

--- approvals.py
+++ approvals.py
@@ -927,3 +927,4 @@ def send_final_confirmation_email(user_email, sponsor, lab):
 scheduler.add_job(check_for_renewals, 'interval', days=3)  # Check every three days
 
 # Test automated PR creation - 2026-02-05
+# Test from Gihub PR should be created without any user input
